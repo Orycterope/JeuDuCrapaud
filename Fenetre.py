a7bf93cf1aa7eea7015c17910d399620fe10adee
Orycterope <tvermeilh@hotmail.fr>
2015-03-20 19:08:40 +0100

    Ajout du fichier de constante et première définition de la fenetre

---
+++ Fenetre.py
@@ -0,0 +1,14 @@
+import pygame
+import Constantes
+
+class Fenetre:
+
+    def __init__(self):
+        pass
+
+    def afficheMenuPricipal(self):
+        pass
+
+    def lancePartie(self, typePartie):
+        pass
+
