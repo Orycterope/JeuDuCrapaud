2d1eb3f2d851e15dc9e2a70e99738e1fe6ab7b3f
Anis <crylegend95@gmail.com>
2015-03-21 07:56:37 +0100

    Déplacement avec le pavé numérique

--- tests/Test.py
+++ tests/Test.py
@@ -9,9 +9,10 @@ Created on 26 févr. 2015
 import pygame
 from pygame.locals import *
 
-def move():
+def move(x, y):
     global position_perso
-    position_perso = position_perso.move(-1, 0)
+    position_perso = position_perso.move(x * 16, y * 16)
+    refresh()
 
 def refresh():
     fenetre.blit(fond, (0,0))   
@@ -21,7 +22,7 @@ def refresh():
 print("Hello World!")
 
 pygame.init()
-pygame.key.set_repeat(1, 1)
+#pygame.key.set_repeat(1, 1)
 pygame.display.set_caption('Tests')
 fenetre = pygame.display.set_mode((640, 480))
 
@@ -42,15 +43,19 @@ while continuer:
         if event.type == QUIT:
             continuer = False
         elif event.type == KEYDOWN:
-            if event.key == K_UP:
-                position_perso = position_perso.move(0, -1)
-                refresh()
-            if event.key == K_DOWN:
-                position_perso = position_perso.move(0, 1)
-                refresh()
-            if event.key == K_LEFT:
-                position_perso = position_perso.move(-1, 0)
-                refresh()
-            if event.key == K_RIGHT:
-                position_perso = position_perso.move(1, 0)
-                refresh()
+            if event.key == K_KP1:
+                move(-1, 1)
+            if event.key == K_KP2:
+                move(0, 2)
+            if event.key == K_KP3:
+                move(1, 1)
+            if event.key == K_KP4:
+                move(-2, 0)
+            if event.key == K_KP6:
+                move(2, 0)
+            if event.key == K_KP7:
+                move(-1, -1)
+            if event.key == K_KP8:
+                move(0, -2)
+            if event.key == K_KP9:
+                move(1, -1)
