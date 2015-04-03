d434fd2809343356be6e6939590b717fee1b1120
Orycterope <tvermeilh@hotmail.fr>
2015-04-03 22:37:15 +0200

    2 - 3 modifs, marche pas :/

--- Fenetre.py
+++ Fenetre.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from Constantes import *
 from Controlleur import *
 from pygame.locals import *
@@ -8,10 +9,21 @@ class Fenetre:
 
     def __init__(self):
 
+        pygame.display.set_icon(pygame.image.load("res/kf.png"))
+
         pygame.init()
         pygame.display.set_caption('Jeu du Crapaud')
 
         self.fenetre = pygame.display.set_mode((LARGEUR_FENETRE, HAUTEUR_FENETRE))
+
+        self.fond1 = pygame.image.load("res/background.png").convert_alpha()
+        self.fond2 = pygame.image.load("res/background2.png").convert()
+        self.perso1 = pygame.image.load("res/perso.png").convert_alpha()
+        self.perso2 = pygame.image.load("res/perso2.png").convert_alpha()
+        self.bave = pygame.image.load("res/goutte.png").convert_alpha()
+        self.position_perso1 = self.perso1.get_rect()
+        self.position_perso2 = self.perso2.get_rect()
+        self.bgmenu = pygame.image.load("res/bgmenu.png").convert()
 
         self.afficheMenuPricipal()
 
@@ -42,19 +54,21 @@ class Fenetre:
 
     def refreshMenu(self, hlBlock):
 
-        self.fenetre.blit(self.bgmenu, (10,10))
+        self.fenetre.blit(self.bgmenu, (0,0))
         pass
 
     def afficheMenuPricipal(self):
 
-        pygame.mixer.music.load("res/alex-f.mp3")
+        pygame.mixer.music.load("res/alex-f.mp3") # Menu le plus electro au monde xD
+        pygame.mixer.music.set_volume(0.6) # Sinon les oreilles saignent
         pygame.mixer.music.play(-1)
-        self.bgmenu = pygame.image.load("res/bgmenu.jpg").convert()
+
+
 
         continuer = True
-        highlightedBlock = 2
+        highlightedBlock = PARTIE_DUO # correspond au type de partie qui sera sélectionné
+        self.refreshMenu(highlightedBlock)
         while continuer == True:
-            self.refreshMenu(highlightedBlock)
             for e in pygame.event.get():
                 if e.type == QUIT:
                     self.fermer()
@@ -64,29 +78,22 @@ class Fenetre:
                     if e.key == K_KP6 or e.key == K_RIGHT:
                         highlightedBlock = (highlightedBlock + 1) % 3
                     if e.key == K_KP_ENTER or e.key == K_RETURN:
-                        print("enter")
                         continuer = False
+                    self.refreshMenu(highlightedBlock)
 
         self.lancePartie(highlightedBlock)
 
     def lancePartie(self, typePartie):
 
         pygame.mixer.music.load("res/popcorn.mp3")
+        pygame.mixer.music.set_volume(1) # on remet le volume à donf
         pygame.mixer.music.play(-1) # param -1 fait répéter à l'infini.
-
-        self.fond1 = pygame.image.load("res/background.png").convert()
-        self.fond2 = pygame.image.load("res/background2.png").convert()
-        self.perso1 = pygame.image.load("res/perso.png").convert_alpha()
-        self.perso2 = pygame.image.load("res/perso2.png").convert_alpha()
-        self.bave = pygame.image.load("res/goutte.png").convert_alpha()
-        self.position_perso1 = self.perso1.get_rect()
-        self.position_perso2 = self.perso2.get_rect()
-
 
         Controlleur(typePartie, self)
 
     def fermer(self):
         pygame.quit()
+        sys.exit()
 
 
 
